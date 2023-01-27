e7a94a651f95bb1ac93875518c1637ff32d2c648
unknown <adhirunath@gmail.com>
2023-01-27 11:13:28 +0530

    save

--- Challange_05.java
+++ Challange_05.java
@@ -1,5 +1,36 @@
+import java.util.thread;
+
 class Challange_05{
 
-    
+
+   
+
+    public static void main(String[] args){
+        
+        MainThread basicSal  = new MainThread();
+        basicSal.run();
+        Thread1 allowence = new Thread1();
+        allowence.start();
+
+    }
+
 
 }
+
+class MainThread extends Thread{
+
+public void run(){
+    
+    salary = perDayPayment*noOfDays;
+   }   
+}
+
+class Thread1 extends Thread{
+float allowence =0;
+public float run(float basicSal){
+    
+    allowence = (basicSal/100)*5;
+}
+    
+}
+
